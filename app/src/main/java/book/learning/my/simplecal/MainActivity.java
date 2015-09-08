92a1b3e38cedaa43d5469fa38ddb3fd26a5a0f49
ManuBabu <manubabu2012@yahoo.in>
2015-09-08 09:54:53 +0530

    simple calculator
    Just a demo project.

---
+++ app/src/main/java/book/learning/my/simplecal/MainActivity.java
@@ -0,0 +1,19 @@
+package book.learning.my.simplecal;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+}
